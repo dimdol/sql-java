d84ffc63a88aaee98bf871ef70c10b0437f4641a
dimdol <dimdol@gmail.com>
2019-07-04 22:25:05 +0900

    EQUAL과 NOT_EQUAL Operator 테스트 케이스 추가

---
+++ sql-java/test/com/dimdol/sql/runtime/NotEqualOperatorTest2.java
@@ -0,0 +1,90 @@
+package com.dimdol.sql.runtime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dimdol.example.Examples;
+import com.dimdol.sql.Op;
+import com.dimdol.sql.Sql;
+import com.dimdol.sql.loader.DataLoader;
+
+public class NotEqualOperatorTest2 {
+
+    @BeforeClass
+    public static void setUp() {
+        Examples.configure();
+    }
+
+    @Test
+    public void notEqual() {
+        DataLoader.run(loader -> {
+            loader.table("XZY_USER", table -> {
+                table.column("ID");
+                table.column("NAME");
+                table.record("A", "정대만");
+                table.record("B", "윤대협");
+            });
+        }, () -> {
+            Sql sql = new Sql();
+            sql.select("NAME");
+            sql.from("XZY_USER");
+            sql.where("NAME", Op.NOT_EQUAL, "윤대협");
+            sql.each((i, rs) -> {
+                assertEquals("정대만", rs.getString("NAME"));
+            });
+        });
+    }
+
+    @Test
+    public void subquery() {
+        DataLoader.run(loader -> {
+            loader.table("XZY_USER", table -> {
+                table.column("ID");
+                table.column("NAME");
+                table.record("A", "정대만");
+                table.record("B", "윤대협");
+            });
+        }, () -> {
+            Sql sql = new Sql();
+            sql.select("NAME");
+            sql.from("XZY_USER");
+            sql.where("ID", Op.NOT_EQUAL, subquery -> {
+                subquery.select("ID");
+                subquery.from("XZY_USER");
+                subquery.where("NAME", Op.EQUAL, "정대만");
+            });
+            sql.each((i, rs) -> {
+                assertEquals("윤대협", rs.getString("NAME"));
+            });
+        });
+    }
+
+    @Test
+    public void twoColumnSubquery() {
+        DataLoader.run(loader -> {
+            loader.table("XZY_USER", table -> {
+                table.column("ID");
+                table.column("NAME");
+                table.record("A", "정대만");
+                table.record("A", "윤대협");
+            });
+        }, () -> {
+            Sql sql = new Sql();
+            sql.select("NAME");
+            sql.from("XZY_USER");
+            sql.where("(ID, NAME)", Op.NOT_EQUAL, subquery -> {
+                subquery.select("ID");
+                subquery.select("NAME");
+                subquery.from("XZY_USER");
+                subquery.where("ID", Op.EQUAL, "A");
+                subquery.where("NAME", Op.EQUAL, "정대만");
+            });
+            sql.each((i, rs) -> {
+                assertEquals("윤대협", rs.getString("NAME"));
+            });
+        });
+    }
+
+}
