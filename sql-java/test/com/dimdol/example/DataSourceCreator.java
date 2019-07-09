f43faf958cfecaadf9212a17dc6e6cdcbc871988
dimdol <dimdol@gmail.com>
2019-07-09 20:24:12 +0900

    테스트 속도 향상과 안정성을 위해 ConnectionPool 구현

---
+++ sql-java/test/com/dimdol/example/DataSourceCreator.java
@@ -0,0 +1,23 @@
+package com.dimdol.example;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+public class DataSourceCreator {
+
+    public DataSource setupDataSource(String connectURI, String username, String password) {
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(connectURI, username, password);
+        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, null);
+        ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnectionFactory);
+        poolableConnectionFactory.setPool(connectionPool);
+        return new PoolingDataSource<>(connectionPool);
+    }
+
+}
--- sql-java/test/com/dimdol/example/OracleConnectionFactory.java
+++ sql-java/test/com/dimdol/example/OracleConnectionFactory.java
@@ -1,17 +1,19 @@
 package com.dimdol.example;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import com.dimdol.sql.ConnectionFactory;
 
 public class OracleConnectionFactory implements ConnectionFactory {
 
+    private DataSource ds;
+
     @Override
     public Connection getConnection() throws SQLException {
-        String dbURL = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-        return DriverManager.getConnection(dbURL, "SYSTEM", "1988kim");
+        return getDataSource().getConnection();
     }
 
     @Override
@@ -20,6 +22,14 @@ public class OracleConnectionFactory implements ConnectionFactory {
             throw new IllegalArgumentException();
         }
         return getConnection();
+    }
+
+    private synchronized DataSource getDataSource() {
+        if (ds == null) {
+            String dbURL = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+            ds = new DataSourceCreator().setupDataSource(dbURL, "SYSTEM", "1988kim");
+        }
+        return ds;
     }
 
 }
